f7fe5f15b91106bec8e3258318ca15bbb90d9822
Christina Landvytovych <Crofty.land@gmail.com>
2018-12-12 15:15:59 +0200

    adding confirmation component

---
+++ src/Form/Confirmation.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Button, List } from 'semantic-ui-react';
+
+class Confirmation extends Component{
+    save = (e) => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+
+    back  = (e) => {
+        e.preventDefault();
+        this.props.prevStep();
+    }
+
+    render(){
+        const {values: { firstName, lastName, gender, email, mobile, country, city  }} = this.props;
+
+        return(
+            <div>
+                <h1 className="ui centered">Confirm your Details</h1>
+                <p>Click Confirm if the following details have been correctly entered</p>
+                <List>
+                    <List.Item>
+                        <List.Icon name='users' />
+                        <List.Content>First Name: {firstName}</List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Icon name='users' />
+                        <List.Content>Last Name: {lastName}</List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Icon name='users' />
+                        <List.Content>Gender: {gender}</List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Icon name=' email' />
+                        <List.Content>
+                            <a href='mailto:jack@semantic-ui.com'>{email}</a>
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Icon name='mobile' />
+                        <List.Content>Mobile: {mobile}</List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Icon name='marker' />
+                        <List.Content>{city}, {country}</List.Content>
+                    </List.Item>
+                </List>
+
+                <Button onClick={this.back}>Back</Button>
+                <Button onClick={this.save}>Confirm</Button>
+            </div>
+        )
+    }
+}
+
+export default Confirmation;
