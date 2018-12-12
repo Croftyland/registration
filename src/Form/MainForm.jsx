f4f359ea61f419c5524717cdd1360cf811079c7b
Christina Landvytovych <Crofty.land@gmail.com>
2018-12-12 13:07:05 +0200

    Main form Component

---
+++ src/Form/MainForm.jsx
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+import Avatar from './Avatar';
+import Success from './Success';
+import UserDetails from './UserDatails';
+import Confirmation from './Confirmation';
+import PersonalDetails from './PersonalDetails';
+
+class MainForm extends Component {
+    state = {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        password: '',
+        repeatPassword: '',
+        email: '',
+        mobile: '',
+        gender: '',
+        country: '',
+        city: ''
+    };
+
+    nextStep = () => {
+        const {step} = this.state
+        this.setState({
+            step: step + 1
+        })
+    };
+
+    prevStep = () => {
+        const {step} = this.state
+        this.setState({
+            step: step - 1
+        })
+    };
+
+    handleChange = input => event => {
+        this.setState({[input]: event.target.value})
+    }
+
+
+    render() {
+        const {step} = this.state;
+        const { firstName, lastName, password, repeatPassword, gender, email, mobile, country, city  } = this.state;
+        const values = { firstName, lastName, password, repeatPassword, gender, email, mobile, country, city  };
+        switch(step){
+            case 1:
+                return <UserDetails
+                    nextStep={ this.nextStep }
+                    handleChange = { this.handleChange }
+                    values = { values }
+                />
+            case 2:
+                return <PersonalDetails
+                    nextStep={ this.nextStep }
+                    handleChange = { this.handleChange }
+                    values = { values }
+                />
+            case 3:
+                return <Avatar
+                    nextStep={ this.nextStep }
+                    handleChange = { this.handleChange }
+                    values = { values }
+                />
+            case 4:
+                return <Confirmation
+                    nextStep={ this.nextStep }
+                    handleChange = { this.handleChange }
+                    values = { values }
+                />
+            case 5:
+                return <Success/>
+        }
+
+
+    }
+}
+
+export default MainForm;
