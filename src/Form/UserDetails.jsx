311b622facae9b7462876bc620a2ce04a68c0cd5
Christina Landvytovych <Crofty.land@gmail.com>
2018-12-12 13:57:24 +0200

    User details Component

---
+++ src/Form/UserDetails.jsx
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import { Form, Button } from "semantic-ui-react";
+
+export default class UserDetails extends Component {
+
+    save = (e) => {
+        e.preventdefault()
+        this.props.nextStep()
+    }
+
+    render() {
+        const { values } = this.props;
+        return (
+            <Form>
+                <Form.Field>
+                    <label>First Name</label>
+                    <input
+                    placeholder='First Name'
+                    onChange={this.props.handleChange('firstName')}
+                    defaultValue={values.firstName}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input
+                        placeholder='Last Name'
+                        onChange={this.props.handleChange('lastName')}
+                        defaultValue={values.lastName}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Password</label>
+                    <input
+                        placeholder='Password'
+                        onChange={this.props.handleChange('password')}
+                        defaultValue={values.password}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>RepeatPassword</label>
+                    <input
+                        placeholder='RepeatPassword'
+                        onChange={this.props.handleChange('repeatPassword')}
+                        defaultValue={values.repeatPassword}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>RepeatPassword</label>
+                    <input type="radio"
+                           name="gender"
+                           id="male"
+                           value="male"/>
+                        <label className="form-check-label" htmlFor="male">Male</label>
+                    <input className="form-check-input"
+                           type="radio"
+                           name="gender"
+                           id="female"
+                           value="female"/>
+                    <label className="form-check-label" htmlFor="female">Female</label>
+                </Form.Field>
+                <Button onClick={this.save}>Next</Button>
+            </Form>
+        );
+    }
+}
