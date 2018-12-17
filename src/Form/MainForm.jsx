678a8c1b4b5823baff0df9682f3e1d336960d17e
Christina Landvytovych <Crofty.land@gmail.com>
2018-12-17 23:24:29 +0200

    refactoring validation function

--- src/Form/MainForm.jsx
+++ src/Form/MainForm.jsx
@@ -8,55 +8,55 @@ import Confirmation from './Confirmation';
 import PersonalDetails from './PersonalDetails';
 
 class MainForm extends Component {
-constructor() {
-    super();
-    this.state = {
-        step: 3,
-        values: {
-            firstName: '',
-            lastName: '',
-            password: '',
-            repeatPassword: '',
-            email: '',
-            mobile: '',
-            gender: 'male',
-            country: '',
-            city: ''
-        },
-        errors: {
-            step: 1,
-            firstName: false,
-            lastName: false,
-            password: false,
-            repeatPassword: false,
-            email: false,
-            mobile: false,
-            gender: false,
-            country: false,
-            city: false
-        },
-        activeStep: 1,
-        isDisablePrevBtn: true,
-        isDisableNextBtn: true
+    constructor() {
+        super();
+        this.state = {
+            step: 3,
+            values: {
+                firstName: '',
+                lastName: '',
+                password: '',
+                repeatPassword: '',
+                email: '',
+                mobile: '',
+                gender: 'male',
+                country: '',
+                city: ''
+            },
+            errors: {
+                step: 1,
+                firstName: false,
+                lastName: false,
+                password: false,
+                repeatPassword: false,
+                email: false,
+                mobile: false,
+                gender: false,
+                country: false,
+                city: false
+            },
+            activeStep: 1,
+            isDisablePrevBtn: true,
+            isDisableNextBtn: true
+        }
     }
-}
 
     onChange = e => {
         const name = e.target.name;
         const value = e.target.value;
         this.setState(prevState => ({
-            values: { ...prevState.values, [name]: value }
+            values: {...prevState.values, [name]: value}
         }));
-        console.log(e.target.name,e.target.value,e.target.checked)
+        console.log(e.target.name, e.target.value, e.target.checked)
     };
     validateFields = () => {
         const errors = {};
         if (this.state.activeStep === 1) {
-            if (this.state.values.firstName.length < 5) {
-                errors.firstName = "Must be more then 4 characters";
+            if (this.state.values.firstname.length < 5) {
+                errors.firstname = "Must be more then 4 characters";
             }
-            if (this.state.values.lastName.length < 5) {
-                errors.lastName = "Must be more then 4 characters";
+            if (this.state.values.lastname.length < 5) {
+                errors.lastname = "Must be more then 4 characters";
             }
             if (this.state.values.password.length < 6) {
                 errors.password = "Must be more then 5 characters";
@@ -67,8 +67,10 @@ constructor() {
             ) {
                 errors.repeatPassword = "Password must be the same";
             }
-        } else if (this.state.activeStep === 2) {
+        }
+        if (this.state.activeStep === 2) {
             const regExpMail = new RegExp("^.+@[^.].*.[a-z]{2,}$");
+
             if (!regExpMail.test(this.state.values.email)) {
                 errors.email = "Invalid email address";
             }
@@ -105,7 +107,7 @@ constructor() {
         const reader = new FileReader();
         reader.onload = event => {
             this.setState({
-                values: { avatar: event.target.result }
+                values: {avatar: event.target.result}
             });
         };
 
@@ -114,11 +116,11 @@ constructor() {
 
 
     render() {
-        const { activeStep, values, errors } = this.state;
+        const {activeStep, values, errors} = this.state;
         return (
             <div className="form-container card">
                 <form className="form card-body">
-                    <Steps activeStep={activeStep} />
+                    <Steps activeStep={activeStep}/>
                     {activeStep === 1 ? (
                         <UserDetails
                             errors={errors}
@@ -152,8 +154,8 @@ constructor() {
                     ) : null}
 
                     <div className="d-flex justify-content-center">
-                        <Button  name="Previous" onChangeStep={this.onChangeStep} />
-                        <Button  name="Next" onChangeStep={this.onChangeStep} />
+                        <Button name="Previous" onChangeStep={this.onChangeStep}/>
+                        <Button name="Next" onChangeStep={this.onChangeStep}/>
                     </div>
                 </form>
             </div>
