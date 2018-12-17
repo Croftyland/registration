2f17df98a300c020693e54a18c5d84dee6af250f
Christina Landvytovych <Crofty.land@gmail.com>
2018-12-17 23:26:14 +0200

    refactoring button function

--- src/Buttons/Button.jsx
+++ src/Buttons/Button.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 
-export default class Button extends React.Component {
+export default class Buttons extends React.Component {
     render() {
-        const { name } = this.props;
-        return <span  onClick={this.props.onChangeStep(name)}>{name}</span>;
+        const { activeStep, onChangeStep } = this.props;
+
+        return (
+            <React.Fragment>
+                {activeStep !== 4 && (
+                    <div className="d-flex justify-content-center">
+                        <button
+                            type="button"
+                            disabled={activeStep === 1}
+                            className="mr-2 ml-2 btn btn-light"
+                            onClick={activeStep !== 1 ? onChangeStep("Previous") : undefined}
+                        >
+                            Previous
+                        </button>
+                        <button
+                            type="button"
+                            className="mr-2 ml-2 btn btn-light"
+                            onClick={activeStep !== 4 ? onChangeStep("Next") : undefined}
+                        >
+                            Next
+                        </button>
+                    </div>
+                )}
+            </React.Fragment>
+        );
     }
 }
